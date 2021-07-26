01088d861f45a0970f117c11d2903b4f4cb8f996
francoBg1 <franco9bernal@gmail.com>
2021-07-25 22:29:39 -0400

    reparando email

--- app/Http/Controllers/Portafolio/EmailsController.php
+++ app/Http/Controllers/Portafolio/EmailsController.php
@@ -23,7 +23,7 @@ class EmailsController extends Controller
     public function addEmail(Request $request)
     {
         DB::table('emails')->insert([
-            'user_id' => Auth::user()->id? : "1",
+            'user_id' =>1,
             'name' => $request->input('name'),
             'email' => $request->input('email'),
             'numero' => $request->input('numero'),
