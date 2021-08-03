71e0d71ba93e7b8188d5aa36517efcaee7540938
francoBg1 <franco9bernal@gmail.com>
2021-08-02 21:53:13 -0400

    a

--- app/Models/Visitas.php
+++ app/Models/Visitas.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Visitas extends Model
 {
     use HasFactory;
+
+    public function getCreatedAtAttribute($value)
+    {
+
+        return Carbon::parse($value)->format('d-m-Y H:i:s');
+    }
 }
