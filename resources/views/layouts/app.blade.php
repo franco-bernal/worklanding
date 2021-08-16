487a7b364926d3d6a42d01fc01e8f4e2dd080a5d
francoBg1 <franco9bernal@gmail.com>
2021-08-16 00:01:53 -0400

    m

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <meta property="og:url" content="https://inndev.cf" />
     <meta property="og:type" content="website" />
     <meta property="og:description" content="Desarrollador web, junior ,Desarrollador de páginas web responsive, one page, landing, páginas con administrador, sistemas. Página de contacto y muestra." />
-    <meta property="og:image" content="{{ json_decode($page->img)->tarjeta_pre_img }}" />
+    <meta property="og:image" content="{{ asset(json_decode($page->img)->tarjeta_pre_img) }}" />
   
         
 
@@ -30,7 +30,7 @@
     <meta name="twitter:title" content="Franco bernal desarrollo de Sitio web">
     <meta name="twitter:description" content="Desarrollador web, junior ,Desarrollador de páginas web responsive, one page, landing, páginas con administrador, sistemas. Página de contacto y muestra.">
     <meta name="twitter:url" content="https://inndev.cf">
-    <meta name="twitter:image" content="{{ json_decode($page->img)->tarjeta_pre_img }}">
+    <meta name="twitter:image" content="{{ asset(json_decode($page->img)->tarjeta_pre_img )}}">
 
     <script type="application/ld+json">
         {
